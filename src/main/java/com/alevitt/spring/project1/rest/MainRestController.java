73a1439b232c5f59284ad02af63948a0f99898b5
Artem Trifonov <artemis.levitt@gmail.com>
2024-09-13 19:16:30 +0200

    Inject custom properties

--- src/main/java/com/alevitt/spring/project1/rest/MainRestController.java
+++ src/main/java/com/alevitt/spring/project1/rest/MainRestController.java
@@ -1,10 +1,23 @@
 package com.alevitt.spring.project1.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MainRestController {
+
+    // inject properties
+    @Value("${player.name}")
+    private String playerName;
+
+    @Value("${group.name}")
+    private String groupName;
+
+    @GetMapping("/userinfo")
+    public String getUserInfo() {
+        return "User: " + playerName + " from group: " + groupName;
+    }
 
     @GetMapping("/")
     public String sayHello() {
